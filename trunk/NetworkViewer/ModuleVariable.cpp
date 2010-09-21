258d559daeae0213abc3dcbc449b27a27505921e
maestro <maestro@4f9238e2-d526-4365-9415-5b534b698af7>
2010-09-21 18:29:30 +0000

    Added GPLv3 header.

--- trunk/NetworkViewer/ModuleVariable.cpp
+++ trunk/NetworkViewer/ModuleVariable.cpp
@@ -3,8 +3,18 @@
      http://introlab.gel.usherbrooke.ca
      Dominic Letourneau, ing. M.Sc.A.
      Dominic.Letourneau@USherbrooke.ca
- */
 
+     This file is part of OpenECoSys/NetworkViewer.
+     OpenECoSys/NetworkViewer is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by the Free Software
+     Foundation, either version 3 of the License, or (at your option) any later version.
+     OpenECoSys/NetworkViewer is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+     You should have received a copy of the GNU General Public License along with
+     OpenECoSys/NetworkViewer. If not, see http://www.gnu.org/licenses/.
+
+ */
 #include "ModuleVariable.h"
 #include <QBuffer>
 #include <sstream>
