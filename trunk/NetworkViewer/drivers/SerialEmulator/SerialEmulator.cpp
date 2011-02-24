6a452f4747d25a94c111a466a4a494f6591aa48a
maestro <maestro@4f9238e2-d526-4365-9415-5b534b698af7>
2011-02-24 16:22:23 +0000

    SerialEmulator prototype

--- trunk/NetworkViewer/drivers/SerialEmulator/SerialEmulator.cpp
+++ trunk/NetworkViewer/drivers/SerialEmulator/SerialEmulator.cpp
@@ -16,14 +16,14 @@
 
  */
 
-#include "SerialBridge.h"
+#include "SerialEmulator.h"
 #include <QDebug>
 #include <QCoreApplication>
 
-static bool SERIAL_BRIDGE_DEVICE_INIT = CANDevice::registerDeviceFactory("SerialBridge",new CANDevice::DeviceFactory<SerialBridge>("COM4;1000000","SerialPort;speed"));
+static bool SERIAL_BRIDGE_DEVICE_INIT = CANDevice::registerDeviceFactory("SerialEmulator",new CANDevice::DeviceFactory<SerialEmulator>("COM4;115200","SerialPort;speed"));
 
 
-SerialBridge::SerialBridge(const char* args)
+SerialEmulator::SerialEmulator(const char* args)
     : m_serialPort(NULL), m_pollTimer(NULL)
 {
 
@@ -32,24 +32,26 @@ SerialBridge::SerialBridge(const char* args)
     initialize(args);
 }
 
-CANDevice::State SerialBridge::initialize(const char* args)
+CANDevice::State SerialEmulator::initialize(const char* args)
 {
 
     QStringList config = QString(args).split(";");
     Q_ASSERT(config.size() == 2);
 
-    PortSettings s;
-    s.BaudRate = (BaudRateType) config[1].toInt();
-    s.DataBits =  DATA_8;
-    s.FlowControl = FLOW_OFF;
-    s.Parity = PAR_NONE;
-    s.StopBits = STOP_1;
-    s.Timeout_Millisec = 0;
+    //Port configuration
+    QextPortSettings s;
+    s.baudRate = (BaudRateType) config[1].toInt();
+    s.dataBits =  DATA_8;
+    s.flowControl = FLOW_OFF;
+    s.parity = PAR_NONE;
+    s.stopBits = STOP_1;
+    s.timeoutMsecs = 0;
     m_serialPort = new QextSerialPort(config[0], s);
+
 
     if (!m_serialPort->open(QIODevice::ReadWrite))
     {
-        qDebug("SerialBridge::initialize() : Cannot open serial port : %s",args);
+        qDebug("SerialEmulator::initialize() : Cannot open serial port : %s",args);
         delete m_serialPort;
         m_serialPort = NULL;
         return CANDevice::CANDEVICE_FAIL;
@@ -57,33 +59,23 @@ CANDevice::State SerialBridge::initialize(const char* args)
     else
     {
         //connect signals
-        qDebug("SerialBridge::initialize() : Opening config : %s, baudrate : %i",args,s.BaudRate);
-#ifdef WIN32
+        qDebug("SerialEmulator::initialize() : Opening config : %s, baudrate : %i",args,s.baudRate);
         connect(m_serialPort,SIGNAL(readyRead()),this,SLOT(serialReadyRead()));
         connect(m_serialPort,SIGNAL(bytesWritten(qint64)),this,SLOT(serialBytesWritten(qint64)));
-#else
-	m_pollTimer = new QTimer(this);
-	connect(m_pollTimer,SIGNAL(timeout()),this,SLOT(serialReadyRead()));
-	m_pollTimer->start(1); //1ms timer
-#endif
-
-
         m_testTimer = new QTimer(this);
         connect(m_testTimer,SIGNAL(timeout()),this,SLOT(testTimer()));
         m_testTimer->start(1000);
-
-
         return CANDevice::CANDEVICE_OK;
     }
 }
 
-CANDevice::State SerialBridge::sendMessage(LABORIUS_MESSAGE &message)
+CANDevice::State SerialEmulator::sendMessage(LABORIUS_MESSAGE &message)
 {
     //BE CAREFUL THIS FUNCTION RUNS IN ANOTHER THREAD
     if (m_serialPort)
     {
 	//We need to post the event so the message is processed in the right thread
-        QCoreApplication::postEvent(this,new SerialBridgeSendEvent(message));
+        QCoreApplication::postEvent(this,new SerialEmulatorSendEvent(message));
 	
 	return CANDevice::CANDEVICE_OK;
     }
@@ -93,7 +85,7 @@ CANDevice::State SerialBridge::sendMessage(LABORIUS_MESSAGE &message)
     }
 }
 
-CANDevice::State SerialBridge::recvMessage(LABORIUS_MESSAGE &message)
+CANDevice::State SerialEmulator::recvMessage(LABORIUS_MESSAGE &message)
 {
     //BE CAREFUL THIS FUNCTION RUNS IN ANOTHER THREAD
     CANDevice::State returnState = CANDevice::CANDEVICE_FAIL;
@@ -109,34 +101,27 @@ CANDevice::State SerialBridge::recvMessage(LABORIUS_MESSAGE &message)
 	
 	if (recvSize > 0)
 	{
-            CANRxMessageBuffer msg;
-            msg = m_recvQueue.front();
+            CANSerialMessage buf;
+            buf = m_recvQueue.front();
             m_recvQueue.pop_front();
 
             //Change the return state
             returnState = CANDevice::CANDEVICE_OK;
 
-            unsigned int SID = (msg.messageWord[0]) & 0x000007FF;
-            unsigned int EID = (msg.messageWord[1] >> 10)  & 0x0003FFFF;
-
-
-            //Here convert the message buffer to a LABORIUS_CAN message
-            message.msg_priority = (SID >> 8) & 0x07;
-            message.msg_type = (SID) & 0xFF;
-            message.msg_boot = (EID >> 16) & 0x03 ;
-            message.msg_cmd = (EID >> 8) & 0xFF;
-            message.msg_dest = (EID) & 0xFF;
-            message.msg_data_length = msg.msgEID.DLC;
+            //Convert data structures
+            message.msg_priority = (buf.pri_boot_rtr >> 5) & 0x07;
+            message.msg_type = buf.type;
+            message.msg_boot = (buf.pri_boot_rtr >> 3) & 0x03;
+            message.msg_cmd = buf.cmd;
+            message.msg_dest = buf.dest;
+            message.msg_remote = (buf.pri_boot_rtr & 0x01);
+            message.msg_data_length = (buf.data_length_iface) & 0x0F;
 
             //copy data
-            for (int i = 0; i < 8; i++)
+            for(unsigned i = 0; i < 8; i++)
             {
-                message.msg_data[i] = msg.data[i];
+               message.msg_data[i] = buf.data[i];
             }
-
-            message.msg_remote = msg.msgEID.RTR;
-            message.msg_filter_hit = msg.msgSID.IFACE;
-            message.msg_dwTime = msg.msgSID.CMSGTS;
 
             //CANDevice::printMessage(message);
 	}
@@ -148,7 +133,7 @@ CANDevice::State SerialBridge::recvMessage(LABORIUS_MESSAGE &message)
     return returnState;
 }
 
-bool SerialBridge::newMessageReady()
+bool SerialEmulator::newMessageReady()
 {
     bool ready = false;
     m_recvQueueMutex.lock();
@@ -157,137 +142,96 @@ bool SerialBridge::newMessageReady()
     return ready;
 }
 
-void SerialBridge::serialReadyRead()
+void SerialEmulator::serialReadyRead()
 {
+
+
     if (m_serialPort)
     {
         //Reading N complete messages
-#ifdef WIN32
+        int available = m_serialPort->bytesAvailable() / sizeof(CANSerialMessage);
 
-        int available = m_serialPort->bytesAvailable() / sizeof(CANRxMessageBuffer);
+        serialBytesIn += available * sizeof(CANSerialMessage);
 
-        serialBytesIn += available * sizeof(CANRxMessageBuffer);
-
-        QByteArray array = m_serialPort->read(sizeof(CANRxMessageBuffer) * available);
+        QByteArray array = m_serialPort->read(sizeof(CANSerialMessage) * available);
 
         if (available <= 0)
         {
-            //qDebug("SerialBridge::serialReadyRead() -> not enough bytes received, returning");
+            //qDebug("SerialEmulator::serialReadyRead() -> not enough bytes received, returning");
             return;
         }
-#else
-
-	int available = 1;
-        QByteArray array = m_serialPort->read(sizeof(CANRxMessageBuffer) * available);
 
 
-	if (array.size() <= 0)
-	{
-            qDebug("SerialBridge::serialReadyRead() -> not enough bytes received, returning");
-            return;
-	}
-
-#endif
-
-        //qDebug("SerialBridge::serialReadyRead() available messages : %i",available);
-	//Let's verify if we have received messages that are well aligned with the size of the
-	//CANRxMessageBuffer data structure size
-	Q_ASSERT(array.size() % sizeof(CANRxMessageBuffer) == 0);
+        Q_ASSERT(array.size() % sizeof(CANSerialMessager) == 0);
 	
-	for (int i = 0; i < array.size(); i+= sizeof(CANRxMessageBuffer))
+        for (int i = 0; i < array.size(); i+= sizeof(CANSerialMessage))
 	{
 
-            CANRxMessageBuffer msg;
-            memcpy((char*) &msg.messageWord[0], &array.data()[i],sizeof(CANRxMessageBuffer));
+            CANSerialMessage msg;
+            memcpy((char*) &msg.messageBytes[0], &array.data()[i],sizeof(CANSerialMessage));
 
-            //qDebug("RECV FROM IFACE %i",msg.msgSID.IFACE);
-
-            m_recvQueueMutex.lock();
-            m_recvQueue.push_back(msg);
-            m_recvQueueMutex.unlock();
-
-            //debugging
-            //qDebug("CANRxMessageBuffer word 0 : 0x%8x",msg.messageWord[0]);
-            //qDebug("CANRxMessageBuffer word 1 : 0x%8x",msg.messageWord[1]);
-            //qDebug("CANRxMessageBuffer word 2 : 0x%8x",msg.messageWord[2]);
-            //qDebug("CANRxMessageBuffer word 3 : 0x%8x",msg.messageWord[3]);
-            //qDebug("CANRxMessageBuffer DATA LENGTH : %i",msg.msgEID.DLC);
+            //Look for START_BYTE and VALIDATE CHECKSUM
+            if ((msg.start_byte == START_BYTE) && (msg.checksum == serial_calculate_checksum(&msg)))
+            {
+                m_recvQueueMutex.lock();
+                m_recvQueue.push_back(msg);
+                m_recvQueueMutex.unlock();
+            }
+            else
+            {
+                qDebug("SerialEmulator::serialReadyRead() - Checksum error found : %i, calculated: %i, start_byte : %i",(int)msg.checksum,(int) serial_calculate_checksum(&msg),(int) msg.start_byte);
+            }
 	}
 	
 	//qDebug("MsgCnt : %i",msgCnt);
     }//if m_serialPort
+
 }
 
-void SerialBridge::serialBytesWritten(qint64 nbBytes)
+void SerialEmulator::serialBytesWritten(qint64 nbBytes)
 {
-    //qDebug("SerialBridge::serialBytesWritten = %i",nbBytes);
+    //qDebug("SerialEmulator::serialBytesWritten = %i",nbBytes);
 }
 
-bool SerialBridge::event(QEvent *event)
+bool SerialEmulator::event(QEvent *event)
 {
 
     if(event->type() == QEvent::User)
     {
 
-        SerialBridgeSendEvent *msgEvent = dynamic_cast<SerialBridgeSendEvent*>(event);
+        SerialEmulatorSendEvent *msgEvent = dynamic_cast<SerialEmulatorSendEvent*>(event);
 
         if (msgEvent && m_serialPort)
         {
             //We have a message to send to the socket...
-            CANTxMessageBuffer CANBuffer;
-
-            memset((char*) &CANBuffer,0,sizeof(CANTxMessageBuffer));
+            CANSerialMessage buf;
 
 
-            unsigned int ID = 0;
-
-            //priority
-            ID |= (((unsigned int)msgEvent->m_message.msg_priority << 26) & 0x1C000000);
-
-            //type
-            ID |= (((unsigned int)msgEvent->m_message.msg_type << 18) & 0x03FC0000);
-
-            //boot
-            ID |= (((unsigned int)msgEvent->m_message.msg_boot << 16) & 0x00030000);
-
-            //cmd
-            ID |= (((unsigned int)msgEvent->m_message.msg_cmd << 8) & 0x0000FF00);
-
-            //dest
-            ID |= (((unsigned int)msgEvent->m_message.msg_dest) & 0x000000FF);
+            memset((char*) &buf,0,sizeof(CANSerialMessage));
 
 
-            CANBuffer.msgSID.SID = (ID >> 18);
-            CANBuffer.msgEID.EID = (ID & 0x0003FFFF);
-
-            //Set the interface (default to 1)
-            CANBuffer.msgSID.IFACE = ETHERNET_BRIDGE_CAN1;
-
-            //Set extended message
-            CANBuffer.msgEID.IDE = 1;
-
-            //Those bits should always be cleared
-            CANBuffer.msgEID.SRR = 0;
-            CANBuffer.msgEID.RB0 = 0;
-            CANBuffer.msgEID.RB1 = 0;
-
-            //Set RTR
-            CANBuffer.msgEID.RTR = msgEvent->m_message.msg_remote;
-
-            //copy data length
-            CANBuffer.msgEID.DLC = msgEvent->m_message.msg_data_length;
+            //Set fields
+            buf.start_byte = START_BYTE;
+            buf.pri_boot_rtr = (msgEvent->m_message.msg_priority << 5) | (msgEvent->m_message.msg_boot << 3) | (msgEvent->m_message.msg_remote);
+            buf.type = msgEvent->m_message.msg_type;
+            buf.cmd = msgEvent->m_message.msg_cmd;
+            buf.dest = msgEvent->m_message.msg_dest;
+            buf.data_length_iface = (msgEvent->m_message.msg_data_length <<4);
 
             //copy data
-            for (int i = 0; i<8; i++)
+            for (unsigned int i = 0 ; i < 8; i++)
             {
-                CANBuffer.data[i] = msgEvent->m_message.msg_data[i];
+                buf.data[i] = msgEvent->m_message.msg_data[i];
             }
 
+            //calculate checksum
+            buf.checksum = serial_calculate_checksum(&buf);
 
-            serialBytesOut += sizeof(CANTxMessageBuffer);
 
-            //Write to socket
-            m_serialPort->write((char*) &CANBuffer, sizeof(CANTxMessageBuffer));
+            serialBytesOut += sizeof(CANSerialMessage);
+
+            //Write to serial port
+            m_serialPort->write((char*) &buf.messageBytes[0], sizeof(CANSerialMessage));
 
             //Make sure the socket is flushed...
             m_serialPort->flush();
@@ -299,7 +243,7 @@ bool SerialBridge::event(QEvent *event)
     return QObject::event(event);
 }
 
- void SerialBridge::testTimer()
+ void SerialEmulator::testTimer()
  {
     qDebug("IN: %li OUT: %li",serialBytesIn,serialBytesOut);
 
