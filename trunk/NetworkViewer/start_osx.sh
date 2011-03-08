44b0cf86c6d15a1cfbb31f9abaced65ec7405988
maestro <maestro@4f9238e2-d526-4365-9415-5b534b698af7>
2011-03-08 21:44:21 +0000

    almost working OSX package

--- trunk/NetworkViewer/start_osx.sh
+++ trunk/NetworkViewer/start_osx.sh
@@ -1,2 +1,12 @@
-export DYLD_LIBRARY_PATH=$PWD
-exec ./NetworkViewer
+#!/bin/sh
+
+NETWORKVIEWER_BUNDLE="`echo "$0" | sed -e 's/\/Contents\/MacOS\/NetworkViewer//'`"
+
+echo "NetworkViewer running from  : $PWD   Bundle : $NETWORKVIEWER_BUNDLE"
+
+export DYLD_LIBRARY_PATH=$NETWORKVIEWER_BUNDLE/Contents/Resources/lib
+export PATH=$NETWORKVIEWER_BUNDLE/Contents/Resources/bin:$PATH
+
+#run
+exec $NETWORKVIEWER_BUNDLE/Contents/Resources/bin/NetworkViewer
+
