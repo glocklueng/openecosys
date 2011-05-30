c247ac1fe586b7690b823b1dcc3a06e9a425b7d2
maestro <maestro@4f9238e2-d526-4365-9415-5b534b698af7>
2011-05-30 17:39:55 +0000

    testing libraries

--- NETVProtocolStack/NETVArduino/NETVArduino.cpp
+++ NETVProtocolStack/NETVArduino/NETVArduino.cpp
@@ -3,8 +3,16 @@
 NETVArduino netvArduino;
 
 NETVArduino::NETVArduino()
+: m_table(NULL), m_size(0)
 {
 	
 }
 
-
+void NETVArduino::setup(byte* table, unsigned int size)
+{
+  if (table && size > 0)
+  {
+       m_table = table;
+       m_size = size;
+  }
+}
