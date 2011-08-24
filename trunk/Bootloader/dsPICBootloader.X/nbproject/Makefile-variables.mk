be418d5904027a8005174cffd2101bab2d1fd73e
maestro <maestro@4f9238e2-d526-4365-9415-5b534b698af7>
2011-08-24 18:51:52 +0000

    Added empty MPLABX project

---
+++ trunk/Bootloader/dsPICBootloader.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=dsPICBootloader.X.production.elf
+CND_ARTIFACT_PATH_default=dist/default/production/dsPICBootloader.X.production.elf
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=dspicbootloader.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/dspicbootloader.x.tar
