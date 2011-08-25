e31e993ae10674f5e3c561573cbcf2a9abe13d28
maestro <maestro@4f9238e2-d526-4365-9415-5b534b698af7>
2011-08-25 17:56:06 +0000

    Starting bootloader code.

--- trunk/Bootloader/dsPICBootloader.X/main.c
+++ trunk/Bootloader/dsPICBootloader.X/main.c
@@ -1,3 +1,26 @@
+/*
+The OpenEcoSys project / NETVProtocolStack
+Copyright (C) 2011  IntRoLab - Universite de Sherbrooke
+
+Author(s)
+
+Dominic Letourneau, ing., M.Sc.A.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #include "bsp.h"
 #include "NETV16_Common.h"
 
@@ -7,14 +30,38 @@ _FWDT(WDT_OFF);
 _FBORPOR(PBOR_OFF & BORV45 & PWRT_64 & MCLR_EN);
 //----------------------------------------------------------------------
 
+// Initial bootloader config in eeprom
+// Variable located in EEPROM
+int __attribute__ ((space(eedata))) g_bootData[8] = {BOOT_IDLE,
+        0x0000, // PROJECT_ID
+        0x0001, // MODULE_ID
+        0x0001, // CODE_VERSION
+        0x0002, // TABLE_VERSION
+        0x0006, // BOOT_DELAY (SECONDS)
+        0x0000, // DEVID LOW
+        0x0000}; //DEVID HIGH
+
+
+//Bootloader commands
+enum {CMD_NOP,CMD_RESET,CMD_START_WRITE,CMD_END_WRITE};
+
+
+
+
 
 void netv_proc_message(NETV_MESSAGE *msg)
 {
 
 }
 
+//This code should be working with multiple interface...
 int main()
 {
+    BootConfig *config = netv_get_boot_config();
+
+    //Read actual configuration
+    netv_read_boot_config(config);
+
     while(1);
 
     return 0;
