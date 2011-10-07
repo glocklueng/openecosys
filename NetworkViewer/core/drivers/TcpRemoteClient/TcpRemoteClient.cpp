d7f8798a07a2c6d8f8756ef29c3728c688f246d4
maestro <maestro@4f9238e2-d526-4365-9415-5b534b698af7>
2011-10-07 18:05:16 +0000

    Skeleton for TcpRemoteClient driver.

---
+++ NetworkViewer/core/drivers/TcpRemoteClient/TcpRemoteClient.cpp
@@ -0,0 +1,61 @@
+/**
+     Copyright (C) 2011 IntRoLab
+     http://introlab.gel.usherbrooke.ca
+     Dominic Letourneau, ing. M.Sc.A.
+     Dominic.Letourneau@USherbrooke.ca
+
+     This file is part of OpenECoSys/NetworkViewer.
+     OpenECoSys/NetworkViewer is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by the Free Software
+     Foundation, either version 3 of the License, or (at your option) any later version.
+     OpenECoSys/NetworkViewer is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+     You should have received a copy of the GNU General Public License along with
+     OpenECoSys/NetworkViewer. If not, see http://www.gnu.org/licenses/.
+
+ */
+
+#include "TcpRemoteClient.h"
+#include <QDebug>
+#include <QCoreApplication>
+
+static bool TCP_REMOTE_CLIENT_DEVICE_INIT = NETVDevice::registerDeviceFactory("TcpRemoteClient",new NETVDevice::DeviceFactory<TcpRemoteClient>("localhost;12345","Host;port"));
+
+
+TcpRemoteClient::TcpRemoteClient(const char *args)
+{
+    if (args)
+    {
+        initialize(args);
+    }
+}
+
+NETVDevice::State TcpRemoteClient::initialize(const char* args)
+{
+    return NETVDevice::NETVDEVICE_FAIL;
+}
+
+NETVDevice::State TcpRemoteClient::sendMessage(NETV_MESSAGE &message)
+{
+
+    return NETVDevice::NETVDEVICE_FAIL;
+}
+
+
+NETVDevice::State TcpRemoteClient::recvMessage(NETV_MESSAGE &message)
+{
+    return NETVDevice::NETVDEVICE_FAIL;
+}
+
+
+bool TcpRemoteClient::newMessageReady()
+{
+    return false;
+}
+
+
+bool TcpRemoteClient::event(QEvent *event)
+{
+
+}
---
+++ NetworkViewer/core/drivers/TcpRemoteClient/TcpRemoteClient.h
@@ -0,0 +1,73 @@
+/**
+     Copyright (C) 2011 IntRoLab
+     http://introlab.gel.usherbrooke.ca
+     Dominic Letourneau, ing. M.Sc.A.
+     Dominic.Letourneau@USherbrooke.ca
+
+     This file is part of OpenECoSys/NetworkViewer.
+     OpenECoSys/NetworkViewer is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by the Free Software
+     Foundation, either version 3 of the License, or (at your option) any later version.
+     OpenECoSys/NetworkViewer is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+     or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+     You should have received a copy of the GNU General Public License along with
+     OpenECoSys/NetworkViewer. If not, see http://www.gnu.org/licenses/.
+
+ */
+
+#ifndef _TCP_REMOTE_CLIENT_H_
+#define _TCP_REMOTE_CLIENT_H_
+
+
+#include "NETVDevice.h"
+#include <QSemaphore>
+#include <QMutex>
+#include <QList>
+#include <QEvent>
+#include "NETVRemoteClient.h"
+
+
+
+class TcpRemoteClient : public QObject, public NETVDevice
+{
+    Q_OBJECT;
+
+public:
+
+    TcpRemoteClient(const char *args);
+
+
+    virtual NETVDevice::State initialize(const char* args);
+
+
+    /** send a NETV_MESSAGE
+         \param message The message to send
+         \return int The status after the message has been sent
+    */
+    virtual NETVDevice::State sendMessage(NETV_MESSAGE &message);
+
+    /** receive a NETV_MESSAGE
+         \param message The message to receive (will be filled)
+         \return int The status after the message has been received
+    */
+    virtual NETVDevice::State recvMessage(NETV_MESSAGE &message);
+
+    /** Verify if a message is ready to receive
+         \return bool true if a message is ready to be received
+    */
+    virtual bool newMessageReady();
+
+public slots:
+
+
+protected:
+
+    //Internal event processing...
+    bool event(QEvent *event);
+
+};
+
+
+#endif
+
