7b2be61f638b078d064d838231612deee3a4746e
maestro <maestro@4f9238e2-d526-4365-9415-5b534b698af7>
2012-06-22 14:31:33 +0000

    prototype send to osc

--- trunk/NetworkViewer/console/src/osc_generator.cpp
+++ trunk/NetworkViewer/console/src/osc_generator.cpp
@@ -121,6 +121,155 @@ void write_variable_interface(const ModuleConfiguration &conf, QTextStream &out,
 }
 
 
+void read_variable_interface(const ModuleConfiguration &conf, QTextStream &out, unsigned int deviceID = 0)
+{
+
+    out<<"//This code is auto-generated. Do not modify.\n";
+    out<<"int sprintf_osc(char* buffer, int length, NETV_MESSAGE *message)\n";
+    out<<"{\n";
+
+    out<<"\tint size=0;\n";
+    out<<"\tswitch(message->msg_cmd)\n";
+    out<<"\t{\n";
+    for(unsigned int i= 0; i < conf.size(); i++)
+    {
+
+        ModuleVariable *var = conf[i];
+        if(var->getMemType() == ModuleVariable::RAM_VARIABLE)
+        {
+            out<<"\t\tcase "<<var->getOffset()<<":\n";
+            QString path = QString("/") + conf.getConfigName() + "/" + QString::number(deviceID) + "/" + var->getName();
+            out << "\t\t\tstrncpy(buffer,\"" << path << "\"," << path.size() << ");\n";
+            out << "\t\t\tbuffer["<<path.size()<<"]=0;\n";
+            out << "\t\t\tbuffer["<<path.size() + 1<<"]=',';\n";
+
+
+            switch(var->getType())
+            {
+            case ModuleVariable::DOUBLE:
+                out << "\t\t\tbuffer["<<path.size() + 2<<"]='d';\n"; //format
+                out << "\t\t\tbuffer["<<path.size() + 3<<"]=0;\n"; //null
+                out << "\t\t\tbuffer["<<path.size() + 4<<"]=message->msg_data[7];\n";
+                out << "\t\t\tbuffer["<<path.size() + 5<<"]=message->msg_data[6];\n";
+                out << "\t\t\tbuffer["<<path.size() + 6<<"]=message->msg_data[5];\n";
+                out << "\t\t\tbuffer["<<path.size() + 7<<"]=message->msg_data[4];\n";
+                out << "\t\t\tbuffer["<<path.size() + 8<<"]=message->msg_data[3];\n";
+                out << "\t\t\tbuffer["<<path.size() + 9<<"]=message->msg_data[2];\n";
+                out << "\t\t\tbuffer["<<path.size() + 10<<"]=message->msg_data[1];\n";
+                out << "\t\t\tbuffer["<<path.size() + 11<<"]=message->msg_data[0];\n";
+                out << "\t\t\tsize="<<path.size() + 11<<";\n";
+                break;
+
+            case ModuleVariable::FLOAT:
+                out << "\t\t\tbuffer["<<path.size() + 2<<"]='f';\n";
+                out << "\t\t\tbuffer["<<path.size() + 3<<"]=0;\n";
+                out << "\t\t\tbuffer["<<path.size() + 4<<"]=message->msg_data[3];\n";
+                out << "\t\t\tbuffer["<<path.size() + 5<<"]=message->msg_data[2];\n";
+                out << "\t\t\tbuffer["<<path.size() + 6<<"]=message->msg_data[1];\n";
+                out << "\t\t\tbuffer["<<path.size() + 7<<"]=message->msg_data[0];\n";
+                out << "\t\t\tsize="<<path.size() + 7<<";\n";
+                break;
+
+            case ModuleVariable::SINT32:
+                out << "\t\t\tbuffer["<<path.size() + 2<<"]='i';\n";
+                out << "\t\t\tbuffer["<<path.size() + 3<<"]=0;\n";
+                out << "\t\t\tbuffer["<<path.size() + 4<<"]=message->msg_data[3];\n";
+                out << "\t\t\tbuffer["<<path.size() + 5<<"]=message->msg_data[2];\n";
+                out << "\t\t\tbuffer["<<path.size() + 6<<"]=message->msg_data[1];\n";
+                out << "\t\t\tbuffer["<<path.size() + 7<<"]=message->msg_data[0];\n";
+                out << "\t\t\tsize="<<path.size() + 7<<";\n";
+                break;
+
+            case ModuleVariable::UINT32:
+                out << "\t\t\tbuffer["<<path.size() + 2<<"]='i';\n";
+                out << "\t\t\tbuffer["<<path.size() + 3<<"]=0;\n";
+                out << "\t\t\tbuffer["<<path.size() + 4<<"]=message->msg_data[3];\n";
+                out << "\t\t\tbuffer["<<path.size() + 5<<"]=message->msg_data[2];\n";
+                out << "\t\t\tbuffer["<<path.size() + 6<<"]=message->msg_data[1];\n";
+                out << "\t\t\tbuffer["<<path.size() + 7<<"]=message->msg_data[0];\n";
+                out << "\t\t\tsize="<<path.size() + 7<<";\n";
+                break;
+
+            case ModuleVariable::SINT16:
+                out << "\t\t\tbuffer["<<path.size() + 2<<"]='i';\n";
+                out << "\t\t\tbuffer["<<path.size() + 3<<"]=0;\n";
+
+
+                //Be careful for sign propagation...
+                out << "\t\t\tif(message->msg_data[1] >> 7) \n";
+                out << "\t\t\t{\n";
+                out << "\t\t\t\tbuffer["<<path.size() + 4<<"]=0xFF;\n";
+                out << "\t\t\t\tbuffer["<<path.size() + 5<<"]=0xFF;\n";
+                out << "\t\t\t}\n";
+                out << "\t\t\telse\n";
+                out << "\t\t\t{\n";
+                out << "\t\t\t\tbuffer["<<path.size() + 4<<"]=0;\n";
+                out << "\t\t\t\tbuffer["<<path.size() + 5<<"]=0;\n";
+                out << "\t\t\t}\n";
+                out << "\t\t\tbuffer["<<path.size() + 6<<"]=message->msg_data[1];\n";
+                out << "\t\t\tbuffer["<<path.size() + 7<<"]=message->msg_data[0];\n";
+                out << "\t\t\tsize="<<path.size() + 7<<";\n";
+                break;
+
+            case ModuleVariable::UINT16:
+                out << "\t\t\tbuffer["<<path.size() + 2<<"]='i';\n";
+                out << "\t\t\tbuffer["<<path.size() + 3<<"]=0;\n";
+                out << "\t\t\tbuffer["<<path.size() + 4<<"]=0;\n";
+                out << "\t\t\tbuffer["<<path.size() + 5<<"]=0;\n";
+                out << "\t\t\tbuffer["<<path.size() + 6<<"]=message->msg_data[1];\n";
+                out << "\t\t\tbuffer["<<path.size() + 7<<"]=message->msg_data[0];\n";
+                out << "\t\t\tsize="<<path.size() + 7<<";\n";
+                break;
+
+            case ModuleVariable::SINT8:
+                out << "\t\t\tbuffer["<<path.size() + 2<<"]='i';\n";
+                out << "\t\t\tbuffer["<<path.size() + 3<<"]=0;\n";
+
+                //Be careful for sign propagation...
+                out << "\t\t\tif(message->msg_data[0] >> 7) \n";
+                out << "\t\t\t{\n";
+                out << "\t\t\t\tbuffer["<<path.size() + 4<<"]=0xFF;\n";
+                out << "\t\t\t\tbuffer["<<path.size() + 5<<"]=0xFF;\n";
+                out << "\t\t\t\tbuffer["<<path.size() + 6<<"]=0xFF;\n";
+                out << "\t\t\t}\n";
+                out << "\t\t\telse\n";
+                out << "\t\t\t{\n";
+                out << "\t\t\t\tbuffer["<<path.size() + 4<<"]=0;\n";
+                out << "\t\t\t\tbuffer["<<path.size() + 5<<"]=0;\n";
+                out << "\t\t\t\tbuffer["<<path.size() + 6<<"]=0;\n";
+                out << "\t\t\t}\n";
+                out << "\t\t\tbuffer["<<path.size() + 7<<"]=message->msg_data[0];\n";
+                out << "\t\t\tsize="<<path.size() + 7<<";\n";
+                break;
+
+            case ModuleVariable::UINT8:
+                out << "\t\t\tbuffer["<<path.size() + 2<<"]='i';\n";
+                out << "\t\t\tbuffer["<<path.size() + 3<<"]=0;\n";
+                out << "\t\t\tbuffer["<<path.size() + 4<<"]=0;\n";
+                out << "\t\t\tbuffer["<<path.size() + 5<<"]=0;\n";
+                out << "\t\t\tbuffer["<<path.size() + 6<<"]=0;\n";
+                out << "\t\t\tbuffer["<<path.size() + 7<<"]=message->msg_data[0];\n";
+                out << "\t\t\tsize="<<path.size() + 7<<";\n";
+                break;
+
+            }
+
+
+            out<<"\t\tbreak;\n";
+        }
+    }
+
+    //default
+    out<<"\t\tdefault:\n";
+    out<<"\t\t\treturn -1;\n";
+    out<<"\t\tbreak;\n";
+
+
+    out<<"\t}\n";
+    out<<"\treturn size;\n";
+    out<<"}\n";
+}
+
 int main(int argc, char* argv[])
 {
     ModuleConfiguration config;
@@ -133,6 +282,7 @@ int main(int argc, char* argv[])
         {
             qDebug("Configuration loaded : %s",argv[1]);
             write_variable_interface(config,qout,QString(argv[2]).toUInt());
+            read_variable_interface(config,qout,QString(argv[2]).toUInt());
         }
 
     }
